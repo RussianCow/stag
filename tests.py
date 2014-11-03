4a23b91b6cd5dc65fac4c755560cdd0a22e2e17a
Sasha Chedygov <sasha1rus@gmail.com>
2014-11-03 12:15:06 -0800

    Fixed typo in README

--- tests.py
+++ tests.py
@@ -10,10 +10,17 @@ def test_basic():
 
 
 def test_render():
-    assert render('') == u'<!DOCTYPE html>'
+    """
+    Tests the `render()` function.
+    """
+    assert render(div(), doctype='') == str(div())
+    assert render(div()) == u'<!DOCTYPE html><div></div>'
 
 
 def test_attributes():
+    """
+    Tests attributes being correctly rendered on elements.
+    """
     # We have to test against both because we can't guarantee the order of the attributes
     outputs = [u'<div id="main" style="display:none"></div>', u'<div style="display:none" id="main"></div>']
     assert str(div({'id': 'main', 'style': 'display:none'})) in outputs
@@ -21,11 +28,17 @@ def test_attributes():
 
 
 def test_classes():
+    """
+    Tests the `classes` helper attribute on elements.
+    """
     assert str(div({'class': 'main'})) == str(div(class_='main')) == u'<div class="main"></div>'
     assert str(div(classes=['content', 'wide'])) == u'<div class="content wide"></div>'
 
 
 def test_self_closing():
+    """
+    Tests to make sure self-closing tags properly raise a ValueError when children are inserted.
+    """
     assert str(img(class_='main')) == u'<img class="main">'
     with pytest.raises(ValueError):
         img(p())
